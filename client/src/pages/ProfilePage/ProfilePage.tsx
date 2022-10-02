e91549ff4b26b5fa2dbf6c1c763a204430a7a947
Metin1 <metin3232@yahoo.com>
2022-10-02 21:49:27 +0300

    feat: profile page

--- client/src/pages/ProfilePage/ProfilePage.tsx
+++ client/src/pages/ProfilePage/ProfilePage.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { isAuthenticatedSelector } from 'store/selectors'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import Box from 'components/Box'
 
 function ProfilePage() {
   const navigate = useNavigate()
@@ -12,7 +13,39 @@ function ProfilePage() {
     navigate('/login')
   }
 
-  return <div>Welcome to the profile page</div>
+  return (
+    <Box
+      display='flex'
+      width='100%'
+      height='80vh'
+      justifyContent='center'
+      alignItems='center'
+    >
+      <Box
+        display='flex'
+        width='100%'
+        maxWidth='300px'
+        height='100%'
+        maxHeight='200px'
+        backgroundColor='card'
+        border='1px solid border'
+        color='text'
+        alignItems='left'
+        flexDirection='column'
+        p={3}
+      >
+        <Box as='h3' py={2}>
+          Welcome to the profile page
+        </Box>
+        <Box>
+          Username: user
+        </Box>
+        <Box>
+          Password: ****
+        </Box>
+      </Box>
+    </Box>
+  )
 }
 
 export default ProfilePage
